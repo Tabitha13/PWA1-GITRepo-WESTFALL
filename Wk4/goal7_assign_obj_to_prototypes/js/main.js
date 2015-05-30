7ca9a9f0a84f09c31a0d87c3fc0e0537066eac41
Tabitha13 <tnwestfall@fullsail.edu>
2015-05-29 20:00:09 -0400

    main

---
+++ Wk4/goal7_assign_obj_to_prototypes/js/main.js
@@ -0,0 +1,36 @@
+/**
+ * Created by the JavaScript Development Team
+ * Class: PWA
+ * Goal: Goal7
+ */
+
+/*
+
+Names display in arrow
+ex: Tabitha, Cameron, Ryan, Ben and Caitlin
+
+jobs in array
+ex: doctor, teacher, mechanic, cop and realtor
+
+actions in array
+ex: eat, sleep, walk, potty and bathe
+
+for loop
+loops 3 times and chooses 3 random names and 3 random jobs = all static
+
+function populateHTML outputs names and jobs on page
+
+queryselectAll = #r1c1 on click name
+
+etc
+
+setInterval(runUpdate, 1000/30) ---- updates the actions every 30 seconds
+
+run update function
+
+
+
+
+
+
+ */
